d8aec8d113798098ed4026740eab9a9f57ed8a0b
hsuperdev <hoantn.superdev@gmail.com>
2020-02-19 02:19:34 +0700

    demo

--- SHOP-PLUS-ONLINE/app/Http/Controllers/ProductController.php
+++ SHOP-PLUS-ONLINE/app/Http/Controllers/ProductController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     public function index() {
-
+        return view('products.admin.list');
     }
 
     public function detail() {
