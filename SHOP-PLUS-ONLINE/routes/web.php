e09af5661945b9c0a6b337f6aef5122b130800f3
LongC1019I1 <bachthanhlong.it@gmail.com>
2020-02-20 14:15:57 +0700

    update, delete cart

--- SHOP-PLUS-ONLINE/routes/web.php
+++ SHOP-PLUS-ONLINE/routes/web.php
@@ -2,9 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-//Route::post('/','UserController@store')->name('user.store');
+//long
+
 Route::get('cart','CartController@index');
 Route::get('add-to-cart/{id}','CartController@addToCart')->name('addToCart');
+
+Route::patch('update-cart', 'CartController@update');
+
+Route::delete('remove-from-cart', 'CartController@remove');
+
+//Route::post('/','UserController@store')->name('user.store');
+
 
 Route::get('/login','LoginController@formLogin')->name('login');
 Route::get('/register','LoginController@formRegister')->name('register');
