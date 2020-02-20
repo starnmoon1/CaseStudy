a19cde27e3386bec081bdc817a86c65ea8f83556
LongC1019I1 <bachthanhlong.it@gmail.com>
2020-02-20 14:21:59 +0700

    update shop cart

--- SHOP-PLUS-ONLINE/resources/views/products/cart.blade.php
+++ SHOP-PLUS-ONLINE/resources/views/products/cart.blade.php
@@ -1,14 +1,19 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
 <head>
-
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{asset('css/slick.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{asset('css/slick-theme.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{asset('css/nouislider.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{asset('css/style.css')}}">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
@@ -16,7 +21,8 @@
 
 </head>
 <body>
-
+@include('partials.header')
+@include('partials.navbar')
     <div class="section">
         <!-- container -->
         <div class="container">
